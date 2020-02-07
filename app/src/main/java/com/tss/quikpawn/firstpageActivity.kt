372f38897948ce188c343eaaa74044c7a15676e8
drafter <drafter.drafter33@gmail.com>
2020-02-07 17:15:05 +0700

    'firstpage'

---
+++ app/src/main/java/com/tss/quikpawn/firstpageActivity.kt
@@ -0,0 +1,12 @@
+package com.tss.quikpawn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class firstpageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firstpage)
+    }
+}
