8880a8d243e035c49c853159f43a4833251bb646
drafter <drafter.drafter33@gmail.com>
2020-02-17 16:52:57 +0700

    'barcode'

--- app/src/main/java/com/tss/quikpawn/BuyActivity.kt
+++ app/src/main/java/com/tss/quikpawn/BuyActivity.kt
@@ -9,18 +9,18 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_buy.*
 
 
 class BuyActivity : AppCompatActivity() {
-    private val IMAGE_CAPTURE_CODE = 1001
-    private val PERMISSION_CODE = 1000
-    var image_uri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy)
+        val code = intent.getStringExtra("barcode")
         //clear sign button
         clearsign_btn.setOnClickListener{
             signature_pad.clear()
@@ -28,55 +28,13 @@ class BuyActivity : AppCompatActivity() {
         //get bitmap signature
         ok_btn.setOnClickListener {
             val Bitmap = signature_pad.getSignatureBitmap()
-
         }
-        barcode_btn.setOnClickListener {
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if (checkSelfPermission(android.Manifest.permission.CAMERA)
-                    == PackageManager.PERMISSION_DENIED ||
-                    checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_DENIED){
-                    val permission = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    requestPermissions(permission,PERMISSION_CODE)
-                }
-                else{
-                    openCamera()
-                }
-            }
+        barcode_btn!!.setOnClickListener {
+            val intent = Intent(this@BuyActivity, ScanActivity::class.java)
+            startActivity(intent)
+        }
+        if(code != null){
+            order_number!!.setText(code)
         }
     }
-    private fun openCamera() {
-        val values = ContentValues()
-        values.put(MediaStore.Images.Media.TITLE,"New Picture")
-        values.put(MediaStore.Images.Media.DESCRIPTION,"From the Camera")
-        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri)
-        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when(requestCode){
-            PERMISSION_CODE -> {
-                if(grantResults.size > 0 && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED){
-                    openCamera()
-                }
-                else{
-                    Toast.makeText(this,"Permission denied", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(resultCode == Activity.RESULT_OK){
-            img_view1.setImageURI(image_uri)
-        }
-    }
-
 }
---
+++ app/src/main/java/com/tss/quikpawn/ScanActivity.kt
@@ -0,0 +1,45 @@
+package com.tss.quikpawn
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.zxing.Result
+import kotlinx.android.synthetic.main.activity_buy.*
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+
+class ScanActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
+
+    private var mScannerView: ZXingScannerView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mScannerView = ZXingScannerView(this)   // Programmatically initialize the scanner view
+        setContentView(mScannerView)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mScannerView!!.setResultHandler(this) // Register ourselves as a handler for scan results.
+        mScannerView!!.startCamera()          // Start camera on resume
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mScannerView!!.stopCamera()           // Stop camera on pause
+    }
+
+    override fun handleResult(rawResult: Result) {
+        // Do something with the result here
+        // Log.v("tag", rawResult.getText()); // Prints scan results
+        // Log.v("tag", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+        val intent = Intent(this@ScanActivity,BuyActivity::class.java)
+        intent.putExtra("barcode",rawResult.text)
+        startActivity(intent)
+//        order_number!!.setText(rawResult.text)
+        onBackPressed()
+
+        // If you would like to resume scanning, call this method below:
+        //mScannerView.resumeCameraPreview(this);
+    }
+}
